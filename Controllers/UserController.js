47eac01940b0ad6954496b01a75917e27cf1af5a
Lightmann9 <obarombitheophilus@gmail.com>
2024-02-22 14:25:53 +0100

    updated 2

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,5 +1,5 @@
 const userModel = require("../Models/UserModel");
-const bcrypt = require("bcrypjs");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const sendMail = require("../Config/mailer");
 
